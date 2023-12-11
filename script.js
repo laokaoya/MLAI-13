e9ac89ede4d14f1b0a5da9ddc49c887479c78d39
laokaoya <140779724+laokaoya@users.noreply.github.com>
2023-12-12 00:17:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ document.getElementById('entrepreneurBtn').addEventListener('click', function() 
     alert('You clicked "I am Entrepreneur"');
     // You can add more logic or redirect to a specific page for entrepreneurs
 });
+
+document.getElementById('entrepreneurBtn').addEventListener('click', function() {
+    // Redirect to the entrepreneur page
+    window.location.href = 'entrepreneur.html';
+});
+
